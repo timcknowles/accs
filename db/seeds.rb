9df80f3de4bb6c443e1bd967ceec25f6ff65daca
Tim Knowles <tim.c.knowles@gmail.com>
2013-09-05 12:08:03 +0100

    curriclum seed and adding collapse

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.delete_all
+File.open("#{Rails.root}/lib/data/curriculum_subl_no_header.txt") do |items|
+  items.read.each_line do |item|
+    code, name = item.split(" ",2)
+    Item.create!(:code => code, :name => name)
+  end
+end
